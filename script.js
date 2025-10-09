3c0bb2409ba130533cd824adbfd8ae97f324a0d3
unknown <winterfell118@gmail.com>
2025-10-09 23:10:51 +0530

    new changes made

--- script.js
+++ script.js
@@ -1,79 +1,72 @@
+function CreateNavbar() {
+  const nav = document.createElement("nav");
+  nav.classList.add("myNav");
+  document.body.appendChild(nav);
+  nav.style.transition = "background-color 0.7s, color 0.10s";
 
+  const ul = document.createElement("ul");
+  ul.classList.add("myUl");
+  nav.appendChild(ul);
 
-function CreateNavbar(){
+  ul.style.transition = "background-color 0.7s, color 0.10s";
 
+  const li1 = document.createElement("li");
+  const li2 = document.createElement("li");
+  const li3 = document.createElement("li");
+  const li4 = document.createElement("li");
+  li1.classList.add("myLi");
+  li2.classList.add("myLi");
+  li3.classList.add("myLi");
+  li4.classList.add("myLi");
 
-const nav = document.createElement("nav");
-nav.classList.add("myNav");
-document.body.appendChild(nav);
-nav.style.transition =  "background-color 0.7s, color 0.10s";
+  ul.append(li1, li2, li3, li4);
 
-const ul = document.createElement("ul");
-ul.classList.add("myUl");
-nav.appendChild(ul);
+  li1.innerText = "Home";
+  li2.innerText = "About";
+  li3.innerText = "Services";
+  li4.innerText = "Contact";
 
-ul.style.transition = "background-color 0.7s, color 0.10s";
-
-const li1 = document.createElement("li");
-const li2 = document.createElement("li");
-const li3 = document.createElement("li");
-const li4 = document.createElement("li");
-li1.classList.add("myLi");
-li2.classList.add("myLi");
-li3.classList.add("myLi");
-li4.classList.add("myLi");
-
-ul.append(li1,li2,li3,li4);
-
-li1.innerText = "Home";
-li2.innerText = "About";
-li3.innerText = "Services";
-li4.innerText = "Contact";
-
-
-return {nav,ul};
+  return { nav, ul };
 }
 
-function createMainDiv(){
+function createMainDiv() {
+  var div = document.createElement("div");
 
- var div = document.createElement("div");
+  div.classList.add("myDiv");
+  document.body.appendChild(div);
+  //document.body.style.textAlign = "center";
+  div.style.transition = "background-color 0.7s, color 0.10s";
 
-div.classList.add("myDiv");
-document.body.appendChild(div);
-//document.body.style.textAlign = "center";
-div.style.transition =  "background-color 0.7s, color 0.10s";
+  const h1 = document.createElement("h1");
+  h1.classList.add("h1");
+  div.appendChild(h1);
+  h1.innerHTML = "HELLO THERE <br>SOURAV SHARMA HERE";
+  h1.style.transition = "transform 0.6s";
+  h1.style.transformStyle = "preserve-3d";
 
-const h1 = document.createElement("h1");
-h1.classList.add("h1");
-div.appendChild(h1);
-h1.innerHTML = "HELLO THERE <br>SOURAV SHARMA HERE"
-h1.style.transition = "transform 0.6s"
-h1.style.transformStyle = "preserve-3d";
+  h1.addEventListener("click", () => {
+    const originalText = h1.innerHTML;
+    h1.style.transform = "rotateX(160deg)";
+    setTimeout(() => {
+      h1.innerHTML = "A FRONTEND DEVELOPER";
+    }, 300);
 
-h1.addEventListener("click",()=> {
-const originalText = h1.innerHTML;
-h1.style.transform = "rotateX(160deg)";
-setTimeout(()=>{
+    setTimeout(() => {
+      h1.style.transform = "rotateX(0deg)";
+      h1.innerHTML = originalText;
+    }, 3000);
+  });
 
-h1.innerHTML = "A FRONTEND DEVELOPER"
-},300)
+  var image = document.createElement("img");
+  image.classList.add("Myimg");
+  image.src = "src/hero-img.png";
+  image.alt = "wooww its not loading";
+  div.appendChild(image);
 
-
-setTimeout(()=>{
-
-h1.style.transform = "rotateX(0deg)" 
-h1.innerHTML = originalText;
-
-},5000 )
-
-
-
-
-
-
-}
-)
-
+  var p = document.createElement("p");
+  p.classList.add("myP");
+  div.appendChild(p);
+  p.innerHTML = "with a passion <br> for developing modern <br> web apps for <br> commercial businesses ";
 
 
 
@@ -83,105 +76,75 @@ h1.innerHTML = originalText;
 
 
 
-
-
-var image = document.createElement("img");
-image.classList.add("Myimg");
-image.src= "src/hero-img.png";
-image.alt="wooww its not loading";
-div.appendChild(image);
-
-
-
-
-
-var p = document.createElement("p");
-p.classList.add("myP");
-div.appendChild(p);
-p.innerHTML="provide the work "
-
-
-
-
-
-
-
-return {div}
-//div.addEventListener("click",function(){
-   // div.style.backgroundColor = "black";
-    //div.style.color = "white";
-//if(div.style.backgroundColor == "black"){
+  return { div };
+  //div.addEventListener("click",function(){
+  // div.style.backgroundColor = "black";
+  //div.style.color = "white";
+  //if(div.style.backgroundColor == "black"){
   //  div.style.backgroundColor = "white";
-    //div.style.color = "black";
-//}else{
+  //div.style.color = "black";
+  //}else{
 
   //  div.style.backgroundColor = "black";
-    //div.style.color = "white";
+  //div.style.color = "white";
 
-//}
+  //}
 
-
-//});
-
-
-
-
-
+  //});
 }
 
-
-
-
-
-
-
-
-function MyFunction(parentDiv,nav,ul){
-
-
-
-var btn = document.createElement("button");
-btn.classList.add("myBtn");
-btn.innerText = "toggle";
-parentDiv.appendChild(btn);
-btn.addEventListener("click",function
-    (){
-
-if(parentDiv.style.backgroundColor == "black"){
-    parentDiv.style.backgroundColor = "white";
-    parentDiv.style.color = "black";
-    nav.style.backgroundColor ="white"
-    ul.style.color = "black";
-    btn.style.color = "white";
-    btn.style.backgroundColor ="black"
-}else{
-parentDiv.style.backgroundColor = "black";
-parentDiv.style.color = "white";
-nav.style.backgroundColor = "black";
-//div.innerText = "SEE THE REAL PROGRESS THROUGH THE DARKNESS";
-ul.style.color = "white";
-btn.style.color = "black";
-btn.style.backgroundColor = "white"
-
-
+function MyFunction(parentDiv, nav, ul) {
+  var btn = document.createElement("button");
+  btn.classList.add("myBtn");
+  btn.innerText = "toggle";
+  parentDiv.appendChild(btn);
+  btn.addEventListener("click", function () {
+    if (parentDiv.style.backgroundColor == "black") {
+      parentDiv.style.backgroundColor = "white";
+      parentDiv.style.color = "black";
+      nav.style.backgroundColor = "white";
+      ul.style.color = "black";
+      btn.style.color = "white";
+      btn.style.backgroundColor = "black";
+    } else {
+      parentDiv.style.backgroundColor = "black";
+      parentDiv.style.color = "white";
+      nav.style.backgroundColor = "black";
+      //div.innerText = "SEE THE REAL PROGRESS THROUGH THE DARKNESS";
+      ul.style.color = "white";
+      btn.style.color = "black";
+      btn.style.backgroundColor = "white";
     }
-//div.appendChild(btn);
-}
-)
-
-
-
-
+    //div.appendChild(btn);
+  });
 }
 
-function initWebsite(){
 
 
-const {nav,ul} = CreateNavbar();
-const {div} = createMainDiv();
 
-MyFunction(div,nav,ul)
 
+
+
+
+
+
+
+
+
+
+
+function initWebsite() {
+  const { nav, ul } = CreateNavbar();
+  const { div } = createMainDiv();
+
+  MyFunction(div, nav, ul);
 }
-
 initWebsite();
+
+
+
+
+
+ const secondDiv = document.getElementById("second");
+const mainDiv = document.querySelector(".myDiv");
+mainDiv.appendChild(secondDiv);
